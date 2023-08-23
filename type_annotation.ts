79bf12b489875147b1be666656505e32fe50bab3
Kumari Ranjana Yadav <yranjana757@gmail.com>
2023-08-23 12:50:35 +0530

    intiail commit

---
+++ type_annotation.ts
@@ -0,0 +1,13 @@
+//? TypeScript - Type Annotations
+
+//* TypeScript is a typed language, where we can specify the type of the variables, function parameters and object properties. We can specify the type using :Type after the name of the variable, parameter or property. There can be a space after the colon. TypeScript includes all the primitive types of JavaScript- number, string and boolean.
+
+
+//! EAXMPLE ......
+//* var age: number = 32; // number variable
+//* var name: string = "John"; // string variable
+//* var isUpdated: boolean = true; // Boolean variable
+
+
+
+
